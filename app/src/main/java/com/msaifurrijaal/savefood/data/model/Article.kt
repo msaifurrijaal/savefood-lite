f5113a7de59d07b34d1c87e7f74dd16d7f68c4c2
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2023-10-12 20:02:34 +0700

    add article and voucher page

--- app/src/main/java/com/msaifurrijaal/savefood/data/model/Article.kt
+++ app/src/main/java/com/msaifurrijaal/savefood/data/model/Article.kt
@@ -1,8 +1,13 @@
 package com.msaifurrijaal.savefood.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Article(
     var title : String = "",
     var imgProfileUser : String = "",
     var photo : Int = 0,
-    var nameWriter : String = ""
-)
+    var nameWriter : String = "",
+    var desc: String = ""
+): Parcelable
